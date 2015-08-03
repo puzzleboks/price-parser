a711ab940f7ec7176434b19f27b391adc4fff8f8
Adrian Shepelavey <adrianshepelavey@gmail.com>
2015-08-03 11:32:24 -0400

    storescontroller code for devise

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,2 +1,49 @@
 class StoresController < ApplicationController
+  # before any post action happens, it will authenticate the user
+    before_action :authenticate_user!
+
+    # another devise helper method that retrieves the user object that has been authenticated
+    def index
+      @stores = current_user.stores
+    end
+
+    def show
+      @store = Store.find(params[:id])
+    end
+
+    def new
+      @store = Store.new
+    end
+
+    def edit
+      @store = Store.find(params[:id])
+    end
+
+    def create
+      @store = current_user.stores.new(store_params)
+      if @store.save
+        redirect_to @store
+      else render 'new'
+      end
+    end
+
+    def update
+      @store = Store.find(params[:id])
+      if @store.update(store_params)
+        redirect_to @store
+      else
+        render 'edit'
+      end
+  	end
+
+    def destroy
+      @store = Store.find(params[:id])
+      @store.destroy
+      redirect_to stores_path
+      end
+    private
+    def store_params
+      params.require(:user).permit(:body)
+    end
+
 end
