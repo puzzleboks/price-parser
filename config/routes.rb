abd2118e55b58c6baf6e56c956f6cd05b3a6b21a
Adrian Shepelavey <adrianshepelavey@gmail.com>
2015-08-04 11:22:18 -0400

    added items to config routes and tweaked stores index html

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   resources :users do
     resources :stores
   end
+
+  resources :stores do
+    resources :items
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
